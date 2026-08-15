434fe7ec6608a55dca9fbc963ad62fcba4acd0f0
David <david.baeza.c@gmail.com>
2026-08-15 00:55:29 -0400

    Blindar conexion de supabase para compilacion

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Traemos las llaves secretas que guardaste en el archivo .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
-// Creamos y exportamos la conexión para usarla en toda la página
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
